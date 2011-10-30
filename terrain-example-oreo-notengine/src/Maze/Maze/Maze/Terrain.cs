5f0c9c446d482a457dc5d4b6786dbda38b68d251
Adrian Collado <acollado@citlink.net>
2011-10-29 23:49:38 -0400

    terrain-add2nd

---
+++ terrain-example-oreo-notengine/src/Maze/Maze/Maze/Terrain.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Maze
+{
+    class Terrain
+    {
+        GraphicsDevice device;
+        GraphicsDeviceManager graphics;
+        Effect effect;
+        VertexPositionColorNormal[] vertices;
+        MazeGame instance;
+
+        Matrix viewMatrix;
+        Matrix projectionMatrix;
+        Matrix worldMatrix;
+
+        int[] indices;
+
+        private int terrainWidth;
+        private int terrainHeight;
+
+        private float[,] heightData;
+
+        private float angle = 0f;
+        private float angle2 = 0f;
+
+        VertexBuffer myVertexBuffer;
+        IndexBuffer myIndexBuffer;
+
+        public struct VertexPositionColorNormal
+        {
+            public Vector3 Position;
+            public Color Color;
+            public Vector3 Normal;
+
+            public static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration
+            (
+                new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
+                new VertexElement(sizeof(float) * 3, VertexElementFormat.Color, VertexElementUsage.Color, 0),
+                new VertexElement(sizeof(float) * 3 + 4, VertexElementFormat.Vector3, VertexElementUsage.Normal, 0)
+            );
+        }
+
+        public Terrain(GraphicsDevice gd, GraphicsDeviceManager gdm, Effect e, MazeGame m)
+        {
+            device = gd;
+            graphics = gdm;
+            effect = e;
+            instance = m;
+        }
+
+        public void LoadContent()
+        {
+            Texture2D heightMap = instance.Content.Load<Texture2D> ("heightmap");
+            LoadHeightData(heightMap);
+            SetupVertices();
+            SetupCamera();
+            SetupIndices();
+            CalculateNormals();
+
+            CopyToBuffers();
+
+        }
+
+        public void Draw()
+        {
+            RasterizerState rs = new RasterizerState();
+            rs.CullMode = CullMode.None;
+            rs.FillMode = FillMode.Solid;
+            device.RasterizerState = rs;
+
+            worldMatrix = Matrix.CreateTranslation(-terrainWidth / 2.0f, 0, terrainHeight / 2.0f) * (Matrix.CreateRotationY(angle2));
+
+            effect.CurrentTechnique = effect.Techniques["Colored"];
+            effect.Parameters["xView"].SetValue(viewMatrix);
+            effect.Parameters["xProjection"].SetValue(projectionMatrix);
+            effect.Parameters["xWorld"].SetValue(worldMatrix);
+            Vector3 lightDirection = new Vector3(1.0f, -1.0f, -1.0f);
+            lightDirection.Normalize();
+            effect.Parameters["xLightDirection"].SetValue(lightDirection);
+            effect.Parameters["xAmbient"].SetValue(0.1f);
+            effect.Parameters["xEnableLighting"].SetValue(true); 
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.Indices = myIndexBuffer;
+                device.SetVertexBuffer(myVertexBuffer);
+                device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, vertices.Length, 0, indices.Length / 3);
+            }
+        }
+
+        // Misc Functions
+        private void SetupVertices()
+        {
+            float minHeight = float.MaxValue;
+            float maxHeight = float.MinValue;
+            for (int x = 0; x < terrainWidth; x++)
+            {
+                for (int y = 0; y < terrainHeight; y++)
+                {
+                    if (heightData[x, y] < minHeight)
+                        minHeight = heightData[x, y];
+                    if (heightData[x, y] > maxHeight)
+                        maxHeight = heightData[x, y];
+                }
+            }
+
+            vertices = new VertexPositionColorNormal[terrainHeight * terrainWidth];
+            for (int x = 0; x < terrainWidth; x++)
+            {
+                for (int y = 0; y < terrainHeight; y++)
+                {
+                    vertices[x + y * terrainWidth].Position = new Vector3(x, heightData[x,y], -y);
+                    if (heightData[x, y] < minHeight + (maxHeight - minHeight) / 4)
+                        vertices[x + y * terrainWidth].Color = Color.Blue;
+                    else if (heightData[x, y] < minHeight + (maxHeight - minHeight) * 2 / 4)
+                        vertices[x + y * terrainWidth].Color = Color.Green;
+                    else if (heightData[x, y] < minHeight + (maxHeight - minHeight) * 3 / 4)
+                        vertices[x + y * terrainWidth].Color = Color.Brown;
+                    else
+                        vertices[x + y * terrainWidth].Color = Color.White;
+                }
+            }
+        }
+
+        public void Update()
+        {
+            KeyboardState keyState = Keyboard.GetState();
+            if (keyState.IsKeyDown(Keys.Down))
+            {
+                angle -= 0.05f;
+            }
+            if (keyState.IsKeyDown(Keys.Up))
+            {
+                angle += 0.05f;
+            }
+            if (keyState.IsKeyDown(Keys.Left))
+            {
+                angle2 += 0.05f;
+            }
+            if (keyState.IsKeyDown(Keys.Right))
+            {
+                angle2 -= 0.05f;
+            }
+        }
+
+        private void SetupCamera()
+        {
+            viewMatrix = Matrix.CreateLookAt(new Vector3(0, 100, 100), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, device.Viewport.AspectRatio, 1.0f, 300.0f);
+        }
+
+        private void SetupIndices()
+        {
+            indices = new int[(terrainWidth - 1) * (terrainHeight - 1) * 6];
+            int counter = 0;
+            for (int y = 0; y < terrainHeight - 1; y++)
+            {
+                for (int x = 0; x < terrainWidth - 1; x++)
+                {
+                    int lowerLeft = x + y * terrainWidth;
+                    int lowerRight = (x + 1) + y * terrainWidth;
+                    int topLeft = x + (y + 1) * terrainWidth;
+                    int topRight = (x + 1) + (y + 1) * terrainWidth;
+
+                    indices[counter++] = topLeft;
+                    indices[counter++] = lowerRight;
+                    indices[counter++] = lowerLeft;
+
+                    indices[counter++] = topLeft;
+                    indices[counter++] = topRight;
+                    indices[counter++] = lowerRight;
+                }
+            }
+        }
+
+        private void LoadHeightData(Texture2D heightMap)
+        {
+            terrainHeight = heightMap.Height;
+            terrainWidth = heightMap.Width;
+
+            Color[] heightMapColors = new Color[terrainWidth * terrainHeight];
+
+            heightMap.GetData(heightMapColors);
+
+            heightData = new float[terrainWidth, terrainHeight];
+            for (int x = 0; x < terrainWidth; x++)
+                for (int y = 0; y < terrainHeight; y++)
+                    heightData[x, y] = heightMapColors[x + y * terrainWidth].R / 5.0f;
+        }
+
+        private void CalculateNormals()
+        {
+            for (int i = 0; i < vertices.Length; i++)
+                vertices[i].Normal = new Vector3(0, 0, 0);
+
+            for (int i = 0; i < indices.Length / 3; i++)
+            {
+                int index1 = indices[i * 3];
+                int index2 = indices[i * 3 + 1];
+                int index3 = indices[i * 3 + 2];
+
+                Vector3 side1 = vertices[index1].Position - vertices[index3].Position;
+                Vector3 side2 = vertices[index1].Position - vertices[index2].Position;
+                Vector3 normal = Vector3.Cross(side1, side2);
+
+                vertices[index1].Normal += normal;
+                vertices[index2].Normal += normal;
+                vertices[index3].Normal += normal;
+            }
+
+            for (int i = 0; i < vertices.Length; i++)
+                vertices[i].Normal.Normalize();
+        }
+
+        private void CopyToBuffers()
+        {
+            myVertexBuffer = new VertexBuffer(device, VertexPositionColorNormal.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
+            myVertexBuffer.SetData(vertices);
+
+            myIndexBuffer = new IndexBuffer(device, typeof(int), indices.Length, BufferUsage.WriteOnly);
+            myIndexBuffer.SetData(indices);
+        }
+    }
+}
